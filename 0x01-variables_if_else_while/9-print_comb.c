98974fd0049fe085e17ace1739f1d4f2d76a0d47
oogecode <oogecode@gmail.com>
2023-04-24 21:08:36 -0700

    my 10th commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num[10] = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(num[i]);
+	}
+	putchar('\n');
+	return (0);
+}
