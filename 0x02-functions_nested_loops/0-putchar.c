7da7afd6cd9c1fcd9ac022f427425c0e637de7f8
oogecode <oogecode@gmail.com>
2023-04-26 19:51:47 -0700

    my first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
+#include <stdioh.h>
 
 /**
- * main - Prints Holberton as a _putchar.
+ * main - Prints  a _putchar.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
-    return 0;
+	putchar("_");
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return(o);
 }
