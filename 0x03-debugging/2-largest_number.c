a500ca4ff5b441bd934d672a7aff923282f33fa1
oogecode <oogecode@gmail.com>
2023-04-27 13:22:07 -0700

    my 3rd commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,48 +1,4 @@
-#include "main.h"
-
-/**
-* main - Tests function for  positive or negative
-* Return: 0
-*/
-
-int main(void)
-{
-int i;
-
-i = 0;
-positive_or_negative(i);
-
-return (0);
-}
-Iniobong Effiong21:06
 #include <stdio.h>
-
-/**
-* main - causes an infinite loop
-* Return: 0
-*/
-
-int main(void)
-{
-int i;
-
-printf("Infinite loop incoming :(\n");
-
-i = 0;
-/*
- *while (i < 10)
- *{
- *		putchar(i);
- *}
- */
-printf("Infinite loop avoided! \\o/\n");
-
-return (0);
-}
-Iniobong Effiong21:11
-Task 2
-#include "main.h"
-
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
