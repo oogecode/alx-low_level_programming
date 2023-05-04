f4a3ce803a01e4832d24523239ee0be375ef809e
oogecode <oogecode@gmail.com>
2023-05-03 21:05:39 -0700

    my 4th commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,16 +7,12 @@
  */
 void _puts(char *str)
 {
-	int count = 0;
+	int i = 0;
 
-	while (count >= 0)
+	while (str[i] != '\0')
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[count]);
-		count++;
+		putchar(str[i];
+		i++;
 	}
+	putchar('\n');
 }
