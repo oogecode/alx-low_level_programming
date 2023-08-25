9d55bffa1e1ebf806794e501d79d642962e0cec1
oogecode <oogecode@gmail.com>
2023-08-25 15:53:27 -0700

    my first commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,10 @@
 #include "lists.h"
 
 /**
- * print_list - This would print all the elements of a linked list
- * @h: pointer to the list_t list to be  printed
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
  *
- * Return: the number of nodes
+ * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
